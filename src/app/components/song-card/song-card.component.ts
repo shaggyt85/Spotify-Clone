3074e99a9c400b17362e27952d6c72ee81674386
Robert Medina <csoperativo@gmail.com>
2024-06-08 16:08:02 -1000

    chore: Update project configuration and dependencies

---
+++ src/app/components/song-card/song-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-song-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './song-card.component.html',
+  styleUrl: './song-card.component.css'
+})
+export class SongCardComponent {
+
+}
